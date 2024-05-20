847323b18e59a01e0ac1ed3170752253031ef00b
Paloma Rehecho <paloma_re@hotmail.com>
2024-05-20 15:08:12 +0100

    import cors

--- server/app.js
+++ server/app.js
@@ -3,23 +3,28 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
 
-// STATIC DATA
-// Devs Team - Import the provided files with JSON data of students and cohorts here:
-// ...
 
-
-// INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
+const cors = require("cors")
 
 
-// MIDDLEWARE
-// Research Team - Set up CORS middleware here:
-// ...
+
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
+
+
+app.use(
+  cors({
+    // Add the URLs of allowed origins to this array
+    origin: ['http://localhost:5173', 'http://example.com'],
+
+  })
+);
+
 
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
@@ -32,5 +37,5 @@ app.get("/docs", (req, res) => {
 
 // START SERVER
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port http://localhost:${PORT}`);
 });
